545838d5789758842e0b414d0cba8d0c68fe75c3
f01yang <fengkai.yang@iav.de>
2025-03-10 15:56:21 +0100

    p556

--- educa/courses/fields.py
+++ educa/courses/fields.py
@@ -5,7 +5,7 @@ from django.db import models
 class OrderField(models.PositiveIntegerField):
     def __init__(self, for_fields=None, *args, **kwargs):
         self.for_fields = for_fields
-        super.__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def pre_save(self, model_instance, add):
         if getattr(model_instance, self.attname) is None:
